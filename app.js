615c27993f03a6e8cb515cba42cd51d3d7ac58cb
Dustin Venable <Venny44@yahoo.com>
2020-07-14 16:59:00 -0500

    Prepped for Heroku

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const helmet = require ('helmet');
 const app = express();
 
 //Mount all middleware that was installed
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
@@ -59,7 +60,18 @@ app.get('/movie', (req, res) => {
     res.json(response);
 })
 
-const PORT = 8000;
+//Hide Error Messages
+app.use((error, req, res, next) => {
+    let response;
+    if (process.env.NODE_ENV === 'production') {
+        response = {error: { message: 'sever error' }}
+    } else {
+        response = {error}
+    }
+    res.status(500).json(response)
+});
+
+const PORT = process.env.POT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
